0b83e0272d1190e3ef547143269e366fa9071a96
chengcainanjing <chengcainuist@gmail.com>
2017-09-11 19:33:54 +0800

    Update 2017.09.09Pytho编程从入门到实践.py

--- notes/2017.09.09Pytho编程从入门到实践.py
+++ notes/2017.09.09Pytho编程从入门到实践.py
@@ -205,8 +205,59 @@ print "\nthe length of the list: "
 print len(cars)
 
 #遍历列表
+#for循环,注意缩进
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print magician
+
+for magician in magicians:
+ print magician.title()+", that was a great trick!"
+	print "I can't wait to see your next trick, " + magician.title() + ".\n"
+	
+print "Thank you, everyone. That was a great magic show!"
+	
+#创建数值列表
+#range()函数生成一系列数字,从第一个数开始，到达第二个数值时停止，但不输出第二个数
+for value in range(1, 5):
+	print value
+
+#list()函数将range()函数作为参数
+#list()函数将数字转换为一个列表
+numbers = list(range(1, 5))
+print numbers
+
+#range()函数还可以指定步长
+numbers = list(range(2, 11, 2))
+print numbers
+
+#创建一个列表，遍历1到10的平方
+#方法1
+squares = []
+for value in range(1, 11):
+	square = value**2
+	squares.append(square)
+
+print squares
+
+#方法2
+squares = []
+for value in range(1, 11):
+	squares.append(value**2)
+	
+print squares
+
+#对数字列表执行简单的统计计算
+digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print min(digits)
+print max(digits)
+print sum(digits)
+
+#列表解析
+squares = [value**2 for value in range(1, 11)]
+print squares
+
+
+
+
 
 
