85382c44a33d75760325cdc955e9602fb0140575
chengcainanjing <chengcainuist@gmail.com>
2017-09-19 16:04:17 +0800

    Update 2017.09.09Pytho编程从入门到实践.py

--- notes/2017.09.09Pytho编程从入门到实践.py
+++ notes/2017.09.09Pytho编程从入门到实践.py
@@ -2093,7 +2093,9 @@ with open('pi_digits.txt') as file_object:
 '''
 with open('/Users/chengcai/Documents/GitHub/Python/pi_digits.txt') as file_object:
     contents = file_object.read()
+    #使用方法read()读取这个文件的全部内容,并将其作为一个长长的字符串存储在变量contents中
     print contents.rstrip()
+    #方法rstrip()删除字符串末尾的空白
 '''
 
 #逐行读取
@@ -2111,7 +2113,9 @@ filename = 'pi_digits.txt'
 
 with open(filename) as file_object:
     lines = file_object.readlines()
- 
+    #readlines()方法从文件中读取每一行，并将其存储在一个列表中，
+    #接下来，该列表被存储到变量lines中
+    
 for line in lines:
     print line.rstrip()
 '''
@@ -2132,14 +2136,144 @@ print len(pi_string)
 '''
 
 #包含一个百万位的大型文件
+#2017.09.19
+
+#print os.getcwd() #显示当前工作目录
+#在notepad++中，打开文件后显示的是notepad++所在的程序目录
+#在python2.7IDLE中显示的是此文件所在的文件夹目录
+#对于类的导入，可在此文件所在的文件夹中搜索对应的模块，
+#而在下面代码中,open()不可以搜索对应的文件夹
+#具体的细节不了解，存在疑问？？？
+#下面代码，报错：
+#IOError:[Errno 2] No such file or directory: 'digit.txt'
+'''
+#import os
+print os.getcwd()	#显示当前工作目录
+with open('pi_digits.txt','r') as file_object:
+	lines = file_object.readlines()
+	
+pi_string = ''
+for line in lines:
+	pi_string += line.strip()
+	
+print pi_string[:52] + '...'
+print len(pi_string)
+'''	
 
 
+#对上面代码进行改进
+#引入import os模块
+#引入os.path.dirname(__file__)
+'''
+#import os
+print '当前所在工作目录：' + os.getcwd()	#显示当前工作目录
 
+path = os.path.dirname(__file__)	#显示此文件所在目录
+print '当前文件所在目录：' + path
+#对于print输出中文的讨论：
+#在windows，用notepad++时，如果使用utf-8编码，执行时，会显示乱码
+#如果转码为ANSI时，再执行，就显示正常为中文
+
+filename = path + '\pi_million_digits.txt'
+
+with open(filename, 'r') as file_object:
+	lines = file_object.readlines()
+	
+pi_string = ''
+for line in lines:
+	pi_string += line.strip()
+	
+print pi_string[:52] + '...'
+print len(pi_string)
 '''
 
-birthday = input("Enter your birthday, in the form mmddyy: ")
+#圆周率值中包含你的生日吗
+'''
+#import os
+print '当前所在工作目录：' + os.getcwd()	#显示当前工作目录
+
+path = os.path.dirname(__file__)	#显示此文件所在目录
+print '当前文件所在目录：' + path
+#对于print输出中文的讨论：
+#在windows，用notepad++时，如果使用utf-8编码，执行时，会显示乱码
+#如果转码为ANSI时，再执行，就显示正常为中文
+
+filename = path + '\pi_million_digits.txt'
+
+with open(filename, 'r') as file_object:
+	lines = file_object.readlines()
+	
+pi_string = ''
+for line in lines:
+	pi_string += line.strip()
+	
+birthday = raw_input('Enter your birthday, in the from mmddyy: ')
 if birthday in pi_string:
-    print("Your birthday appears in the first million digits of pi!")
+	print 'Your birthday appears in the first million digits of pi!'
 else:
-    print("Your birthday does not appear in the first million digits of pi.")
+	print 'Your birthday does not appear in the first million digits of pi!'
 '''
+	
+	
+#test
+#10-1	
+'''
+#import os
+path = os.path.dirname(__file__)
+print '当前文件所在目录：' + path
+
+filename = 	path + '\pi_digits.txt'
+
+#打印整个文件
+with open(filename) as file_object:
+	contents = file_object.read()
+	print contents.rstrip()
+	
+#打印时遍历文件对象
+with open(filename) as file_object:
+	for line in file_object:
+		print line.strip()
+
+#打印时将各行存储在一个列表中
+with open(filename) as file_object:
+	lines = file_object.readlines()
+	#lines是一个列表，打印出来的每个元素后面带有\n
+	print lines
+	
+	pi_string = ''
+	
+	for line in lines:
+		print line.strip()
+		#这是打印列表中的每一行
+		pi_string += line.strip()
+		#这是将每一行合并起来变成一行
+	print pi_string
+'''
+
+#10-2
+'''
+path = os.path.dirname(__file__)
+print '当前文件所在目录：' + path
+
+filename = 	path + '\pi_digits.txt'
+	
+#打印时遍历文件对象
+lines = []
+with open(filename) as file_object:
+	
+	print '原来的数据： '
+	for line in file_object:
+		print line.rstrip()
+		string = line.replace('1', '2')
+		lines.append(string)
+
+print '替换后的数据：'
+for line in lines:
+	print line.rstrip()
+'''
+
+#写入文件
+
+
+
+
